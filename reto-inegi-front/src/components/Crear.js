dc29bf2f07948e64f35e03792fcb575c0f5e5be1
A01749448 <a01749448@itesm.mx>
2022-10-28 14:55:57 -0500

    Avance en componente crear

--- reto-inegi-front/src/components/Crear.js
+++ reto-inegi-front/src/components/Crear.js
@@ -1,7 +1,12 @@
+import "../styles/Crear.css";
 const Crear = () => {
   return (
     <div>
       <h1>Crear</h1>
+      <div className="crear-card-container">
+        <div className="bot-container">Hola</div>
+        <div className="bot-container">Hola</div>
+      </div>
     </div>
   );
 };
