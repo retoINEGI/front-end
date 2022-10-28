841d2d69e6e16aececacf56ffc5c7642a2a1aa16
A01749448 <a01749448@itesm.mx>
2022-10-28 10:36:07 -0500

    Avance en las rutas de la app

--- reto-inegi-front/src/App.js
+++ reto-inegi-front/src/App.js
@@ -1,10 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Inicio from "./components/Inicio";
 import Menu from "./components/Menu";
+import Crear from "./components/Crear";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <div className="App">
       <Menu />
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/crear" element={<Crear />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </div>
   );
 }
--- reto-inegi-front/src/components/Menu.js
+++ reto-inegi-front/src/components/Menu.js
@@ -1,6 +1,13 @@
+import { NavLink } from "react-router-dom";
+
 const Menu = () => {
   return (
     <div>
+      <nav className="navigation">
+        <NavLink to="/">Inicio</NavLink>
+        <NavLink to="crear">Crear</NavLink>
+        <NavLink to="dashboard">Dashboard</NavLink>
+      </nav>
       <h1>Menu</h1>
     </div>
   );
