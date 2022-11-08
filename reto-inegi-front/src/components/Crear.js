057d07779687494f5771be64356635d45e9bf48f
A01749448 <a01749448@itesm.mx>
2022-11-07 18:59:29 -0600

    Avance Chatbot

--- reto-inegi-front/src/components/Crear.js
+++ reto-inegi-front/src/components/Crear.js
@@ -8,7 +8,6 @@ entrada y a través de un chatbot en Amazon Lex.
 
 
 import "../styles/Crear.css";
-import ejemploChatbot from "../images/ejemplo_chatbot.png";
 import ejemploGrafica from "../images/ejemplo_grafica.png";
 const Crear = () => {
   return (
@@ -17,7 +16,13 @@ const Crear = () => {
       <div className="crear-card-container">
         <div className="bot-container">
           <h1 className="text-format">Bot</h1>
-          <img src={ejemploChatbot} className="image-bot-format" alt="" />
+          <iframe
+            className="bot"
+            title="bot"
+            src="https://widget.kommunicate.io/chat?appId=31c586e71bcb02c23f6f9e05dd21e3a44"
+            allow="microphone; geolocation;"
+            frameborder="0"
+          ></iframe>
         </div>
         <div className="bot-container">
           <h1 className="text-format">Vista Previa del Gráfico</h1>
